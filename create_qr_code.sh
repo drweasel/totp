af24452c9195b97921f7eedc28af42517a82b260
Michael Weitzel <mich@elweitzel.de>
2022-12-31 00:12:04 +0100

    command line handling for QR code script

--- create_qr_code.sh
+++ create_qr_code.sh
@@ -1,33 +1,85 @@
 #!/bin/sh
 
+PRG="$0"
 # see https://github.com/google/google-authenticator/wiki/Key-Uri-Format
 
+usage() {
+  >&2 echo
+  >&2 echo "Usage:"
+  >&2 echo "$PRG
+    -a <SHA256|SHA512>  (default: SHA256)
+    -d <DIGITS>         (default: 6)   
+    -i <ISSUER>         (optional)
+    -p <PERIOD>         (default: 30)
+    -u <ACCOUNT>        (required)
+    <SECRET>            (will be queried if omitted)"
+}
+
+PARGS=$(getopt -a -n ${PRG} -o a:d:i:p:u: -- "$@")
+if test "$?" != "0"; then
+  usage
+  exit 2
+fi
+
+_ALGORITHM=SHA256
+_DIGITS=6
+_PERIOD=30
+_ACCOUNT=
+_ISSUER=
+_SECRET=
+
+eval set -- "$PARGS"
+while :
+do
+  case "$1" in
+    -a) _ALGORITHM="$2"; shift 2;;
+    -d) _DIGITS="$2"; shift 2;;
+    -p) _PERIOD="$2"; shift 2;;
+    -u) _ACCOUNT="$2"; shift 2;;
+    -i) _ISSUER="$2"; shift 2;;
+    --) shift; break;;
+    *) echo "unexpected option $1"; usage; exit 2;;
+  esac
+done
+
+if test -z "$_SECRET"; then
+  _SECRET="$@"
+fi
+
+if test -z "$_ACCOUNT"; then
+  >&2 echo
+  >&2 echo "Error: you have to provide argument -u"
+  usage
+  exit 2
+fi
+
 # if the secret is not given on command line it will be read here:
-if test -z "$1"; then
-  echo -n "enter secret password (press enter for a default one): "
+if test -z "$_SECRET"; then
+  >&2 echo -n "enter secret password (press enter for a random one): "
   read -s SECRET
+  >&2 echo
   if test -z "$SECRET"; then
-    SECRET="Die wahnsinnige Kuh lief unablaessig um den Stall"
+    SECRET="$(head -c 20 /dev/random | base64 -w 0)"
   fi
 else
-  SECRET="$1"
+  SECRET="$_SECRET"
 fi
 
 # type is always totp - hotp not supported by this script
 TYPE=totp
 
 # number of digits to generate; possible values: 6, 7, 8, 9
-DIGITS=8
+DIGITS="$_DIGITS"
 # time period
-PERIOD=30
+PERIOD="$_PERIOD"
 
 # possible values: SHA1, SHA256, SHA512
-ALGORITHM=SHA256
+ALGORITHM="$_ALGORITHM"
 
 # an account (mandatory)
-ACCOUNT="john.doe@acme.com"
+ACCOUNT="$_ACCOUNT"
 # an issuer (optional)
-ISSUER="ACME Co"
+ISSUER="$_ISSUER"
 
 # -----------------------------------------------------------------------------
 
